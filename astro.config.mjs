bbf83a789d813463842e6b4b1b8bb3042fdd2b61
Paulneo <jpaulneo2k@gmail.com>
2024-02-20 21:58:06 -0500

    feature

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: 'www.digitalcerti.com',
+  site: 'https://digitalcerti.netlify.app/',
   image: {
     service: passthroughImageService(),
   },
