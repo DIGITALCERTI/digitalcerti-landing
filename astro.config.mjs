6864091ab2d96d7febd8722ed45293f36274b19f
Paulneo <jpaulneo2k@gmail.com>
2024-02-26 20:32:41 -0500

    add google analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig, passthroughImageService } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
+import partytown from '@astrojs/partytown';
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   site: 'https://www.digitalcerti.com/',
   image: {
     service: passthroughImageService(),
